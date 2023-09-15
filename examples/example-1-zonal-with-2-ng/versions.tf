3aebf0fab46134163eabc7b80e93ce28198ddd79
Boris Popov <malibora@gmail.com>
2023-09-15 14:53:32 +0200

    adres config changes

--- examples/example-1-zonal-with-2-ng/versions.tf
+++ examples/example-1-zonal-with-2-ng/versions.tf
@@ -18,10 +18,8 @@ terraform {
 }
 
 provider "yandex" {
-  #endpoint = "api.nemax.nebius.cloud:443"
-  endpoint = "api.il.nebius.cloud:443"
+  endpoint = "api.nemax.nebius.cloud:443"
   folder_id = "b486jia0s4d3r67rcjfa"
-  token = "t1.9euZz8eQm8jGm8yTyMyXj8mLk8vHiu3rmc_HmZ6dk47JmMqcls6JkYyWlIvl8_d5R29X-e8yN3VE_d3z9zl2bFf57zI3dUT9zef165nPx43NiZOSycucy5qalY6LycaQ7_zF65nPx43NiZOSycucy5qalY6LycaQveuZz8eUyZWcjpOKnJiUnI3KlZmOy7XrhpzRlp6S0ZCPmpGWm9KMmo2Jmo0.5oAQu0AlEvRy4igT019y3J2qmwhw1vbwK1NCeEdYDX9GjRniD6MRU2DNetV7faX3M-qsDlR7vku9cV74PtH3CA"
 }
 
 
